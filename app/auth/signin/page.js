3acb3f1b7483220bbf7e306f301aeba2cdf35ede
Emmanuel Augustine <augustinemekus1997@gmail.com>
2024-11-11 11:32:59 +0100

    changes

--- app/auth/signin/page.js
+++ app/auth/signin/page.js
@@ -1,55 +1,74 @@
- "use client"
+"use client";
 import React from "react";
- import Link from "next/link";
+import Link from "next/link";
 import { FaGoogle } from "react-icons/fa";
-import { FaXTwitter }  from "react-icons/fa6";
-import { signIn,useSession,signOut } from "next-auth/react";
+import { FaXTwitter } from "react-icons/fa6";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-export default function Auth () {
-    const {data:session} = useSession();
-     console.log(session);
-    const router =useRouter();
+export default function Auth() {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-    React.useEffect(() => {
-        if (session?.user) {
-            router.push("/dashboard/borrow")
-        }
-    },[session])
+  React.useEffect(() => {
+    if (session?.user) {
+      router.push("/dashboard/borrow");
+    }
+  }, [session]);
 
-    return (
-        <main className=" min-h-[520px] flex justify-center bg-gradient-to-b from-gray-50 to-gray-300 py-8 px-2">
-            <article>
-               <div className="w-full md:w-[24em] rounded-md bg-white p-4">
-                <h1 className="text-2xl mb-2">Sign into Bills</h1>
-                <p className="text-sm text-gray-600 mb-4">Sign in using ...</p>
+  return (
+    <main className=" min-h-[520px] flex justify-center bg-gradient-to-b from-gray-50 to-gray-300 py-8 px-2">
+      <article>
+        <div className="w-full md:w-[24em] rounded-md bg-white p-4">
+          <h1 className="text-2xl mb-2">Sign into Bills</h1>
+          <p className="text-sm text-gray-600 mb-4">Sign in using ...</p>
 
-                {/* Delete this */}
-                 {/* <button onClick={() => signOut}>Sign out</button>  */}
-                {/* Delete this */}
+          {/* Delete this */}
+          {/* <button onClick={() => signOut}>Sign out</button>  */}
+          {/* Delete this */}
 
-                <form 
-                action={ () => {
-                    signIn("google")
-                    signIn("twitter")
-                }}
-                className="mb-2">
-                    <button type="submit" className="w-full h-[3.2em] flex justify-center items-center gap-2 border-b-2 border-red-500 bg-black rounded-md">
-                        <FaGoogle className="text-green-500 text-2xl"/>
-                        <span className="text-white text-lg">Google account</span>
-                    </button>
-                </form>
-                <form className="mb-2">
-                    <button type="submit" className="w-full h-[3.2em] flex justify-center items-center gap-2 border-b-2 border-gray-500 bg-black rounded-md">
-                        <FaXTwitter className="text-white text-2xl"/>
-                        <span className="text-white text-lg">Twitter account</span>
-                    </button>
-                </form>
+          <form
+            action={() => {
+              signIn("google");
+            }}
+            className="mb-2"
+          >
+            <button
+              type="submit"
+              className="w-full h-[3.2em] flex justify-center items-center gap-2 border-b-2 border-red-500 bg-black rounded-md"
+            >
+              <FaGoogle className="text-green-500 text-2xl" />
+              <span className="text-white text-lg">Google account</span>
+            </button>
+          </form>
+          <form
+            action={() => {
+              signIn("twitter");
+            }}
+            className="mb-2"
+          >
+            <button
+              type="submit"
+              className="w-full h-[3.2em] flex justify-center items-center gap-2 border-b-2 border-gray-500 bg-black rounded-md"
+            >
+              <FaXTwitter className="text-white text-2xl" />
+              <span className="text-white text-lg">Twitter account</span>
+            </button>
+          </form>
 
-                <p className="text-gray-600 text-xs">By clicking on the signin button you confirm that you have read and agree with our <Link href="#" className="text-gray-800 underline">Terms of use</Link> and <Link href="#" className="text-gray-800 underline">Privacy Policy</Link></p> 
-                </div>
-            </article>
-
-        </main>
-    )
+          <p className="text-gray-600 text-xs">
+            By clicking on the signin button you confirm that you have read and
+            agree with our{" "}
+            <Link href="#" className="text-gray-800 underline">
+              Terms of use
+            </Link>{" "}
+            and{" "}
+            <Link href="#" className="text-gray-800 underline">
+              Privacy Policy
+            </Link>
+          </p>
+        </div>
+      </article>
+    </main>
+  );
 }
--- components/HistoryTab.js
+++ components/HistoryTab.js
@@ -1,17 +1,28 @@
-import Link from "next/link"
-export  function HistoryTab ({docId,amount,rate,duration,date,type}) {
-    return (
-        <Link href={`/dashboard/loan-details?doc_id=${docId}`} className="flex flex-col border border-blue-100 rounded-md p-3">
-        <ul className="flex justify-between border-b border-blue-100 pb-2">
-            <li className="font-bold text-2xl text-gray-700">N{amount}</li>
-            <li className="text-xs text-blue-500">{rate}%</li>
-        </ul>
+"use client";
+import { useContext } from "react";
+import { AppContext } from "@/config/context.config";
+import { useRouter } from "next/navigation";
 
-        <ul className="flex justify-between items-center pt-2 ">
-            <li className="text-sm text-gray-700">{duration}</li>
-            <li className="text-sm text-gray-700">{date}</li>
-            <li className="text-sm text-gray-700">{type}</li>
-        </ul>
-    </Link>
-    )
+export function HistoryTab({ docId, amount, rate, duration, date, type }) {
+  const { setLoanDocId } = useContext(AppContext);
+  const router = useRouter
+  return (
+    <div
+      onClick={() => {
+        setLoanDocId(docId);
+        router.push("/dashboard/loan-details")
+      }}
+      className="flex flex-col border border-blue-100 rounded-md p-3 cursor-pointer"
+    > <ul className="flex justify-between border-b border-blue-100 pb-2">
+        <li className="font-bold text-2xl text-gray-700">N{amount}</li>
+        <li className="text-xs text-blue-500">{rate}%</li>
+      </ul>
+
+      <ul className="flex justify-between items-center pt-2 ">
+        <li className="text-sm text-gray-700">{duration}</li>
+        <li className="text-sm text-gray-700">{date}</li>
+        <li className="text-sm text-gray-700">{type}</li>
+      </ul>
+    </div>
+  );
 }
